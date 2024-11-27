0bcc50767e8983670cd97347e0a7d1a08f7dd80f
Jordy Neijhorst <j.a.neijhorst@student.tudelft.nl>
2024-11-27 23:12:42 +0100

    Adding the file to the repo

---
+++ template_project/template-project-v2/template-project/server/src/main/java/server/SomeController.java
@@ -0,0 +1,27 @@
+package server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/")
+public class SomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello Jordy!";
+    }
+
+    @GetMapping("/name/{name}")
+    @ResponseBody
+    public String name(@PathVariable("name") String name, @RequestParam("title") String title){
+        var sb = new StringBuilder("Hello ");
+        if(title != null){
+            sb.append(title).append(' ');
+        }
+        sb.append(name);
+        sb.append("!");
+        return sb.toString();
+    }
+}
